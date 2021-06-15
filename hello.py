3ae3fc712c95785d3d8a31c40c7d3b59a8c3761b
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2021-06-15 22:41:58 +0600

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,18 @@
+x = 9
+print(x)
+y = "Al Nahian Ark"
+print(y)
+
+x1, x2, x3 = "Al", "Tony", "Stark"
+print(x3)
+
+
+# memory study
+
+value1 = 100
+value2 = 200
+print(id(value1))
+print(id(value2))
+
+vlaue3 = "AL Nahian Ark"
+print(id(vlaue3))
